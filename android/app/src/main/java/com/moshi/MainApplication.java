32c89ae097094878dc667327b1224248a4adf4dc
wei fuchuan <fuchuan.wei@hotmail.com>
2019-02-18 20:19:16 +0800

    update

--- android/app/src/main/java/com/moshi/MainApplication.java
+++ android/app/src/main/java/com/moshi/MainApplication.java
@@ -3,7 +3,6 @@ package com.moshi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.rnfs.RNFSPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -32,7 +31,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-            new PickerPackage(),
                     new RNFSPackage(),
                     new RNCWebViewPackage(),
                     new RNFetchBlobPackage(),
