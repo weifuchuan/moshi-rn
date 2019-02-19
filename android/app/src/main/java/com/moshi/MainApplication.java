317cf6bd93b8ef3951a3e5450c3171f410f13db1
wei fuchuan <fuchuan.wei@hotmail.com>
2019-02-19 13:36:24 +0800

    update

--- android/app/src/main/java/com/moshi/MainApplication.java
+++ android/app/src/main/java/com/moshi/MainApplication.java
@@ -3,6 +3,7 @@ package com.moshi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.rnfs.RNFSPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new RNDeviceInfo(),
                     new RNFSPackage(),
                     new RNCWebViewPackage(),
                     new RNFetchBlobPackage(),
